cad3b2efc2a740b9d340c85496fd9899c70651a2
wayne <kuderge2002@mail.ru>
2025-03-23 21:15:15 +0300

    add tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Todo app</h1>
+    <form @submit.prevent="addTodo">
+      <input type="text" v-model="newTodo" placeholder="Add task" required />
+      <button type="submit">{{ editIndex !== null ? 'Save' : 'Add' }}</button>
+    </form>
+    <ul>
+      <todo-item v-for="(todo, index) in todos" :key="index" :todo="todo" :index="index" @toggle-todo="toggleTodo"
+        @edit-todo="startEditTodo" @removeTodo="removeTodo" />
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from './components/TodoItem.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoItem
+  },
+  data() {
+    return {
+      newTodo: '',
+      todos: [],
+      editIndex: null
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim() !== '') {
+        if (this.editIndex !== null) {
+          this.todos[this.editIndex].text = this.newTodo;
+          this.editIndex = null;
+        } else {
+          this.todos.push({ text: this.newTodo, completed: false });
+        }
+        this.newTodo = '';
+        this.saveTodos();
+      }
+    },
+    toggleTodo(index) {
+      this.todos[index].completed = !this.todos[index].completed;
+      this.saveTodos();
+    },
+    startEditTodo(index) {
+      this.newTodo = this.todos[index].text;
+      this.editIndex = index;
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+      this.saveTodos();
+    },
+    saveTodos() {
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+    },
+    loadTodos() {
+      const savedTodos = localStorage.getItem('todos');
+      if (savedTodos) {
+        this.todos = JSON.parse(savedTodos);
+      }
+    },
+  },
+  created() {
+    this.loadTodos();
   }
-}
+};
+
 </script>
 
 <style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,29 @@
+<template>
+    <li>
+        <span @click="toggleTodo" :style="{ textDecoration: todo.completed ? 'line-though' : 'none' }">{{ todo.text}}</span>
+        <!--------->
+        <button @click="editTodo">Edit</button>
+        <button @click="removeTodo">Remove</button>
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'TodoItem',
+    props: {
+        todo: Object,
+        index: Number
+    },
+    methods: {
+        toggleTodo() {
+            this.$emit('toggle-todo', this.index);
+        },
+        editTodo() {
+            this.$emit('edit-todo', this.index);
+        },
+        removeTodo() {
+            this.$emit('remove-todo', this.index);
+        },
+    },
+}
+</script>
